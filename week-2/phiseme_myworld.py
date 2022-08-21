32debda60b82d642f535c4430ace084f4dd70d64
Manel Phiseme <mawebsite@yahoo.com>
2022-08-21 16:35:16 -0500

    Week 2 assignment

---
+++ week-2/phiseme_myworld.py
@@ -0,0 +1,10 @@
+# Title: phiseme_myworld.py
+#Author: Manel Phiseme
+#Date: 8/21/2022
+#Description: Introduction to Python
+
+#assign last name to variable last_name
+last_name = "Phiseme's"
+
+#This code print statement within parenthesis to terminal
+print("You are now in " + last_name + " world!" )
