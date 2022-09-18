6367ef32ac719e9e8f2c261eb010c0095e0d448e
Manel Phiseme <mawebsite@yahoo.com>
2022-09-18 17:17:33 -0500

    new files

---
+++ week-6/phiseme-Python with Mongodb.js
@@ -0,0 +1,88 @@
+/**
+	Title: phiseme-python with mongodb.js
+    Author: Professor Krasso
+    Modified By: Manel Phiseme
+    Date: 14 August 2022
+    Description: MongoDB Shell aggregate Queries 
+    for the houses and students collections.
+ */
+
+//query to load the house.js file to the web335db database
+//on mongoose 
+load("houses.js")
+
+//query to show a list of documents in the houses collections
+db.houses.find()
+
+//query to show a list of documents in the student's collections
+db.students.find()
+
+//query to add a document to the student's collections
+db.students.insertOne(
+    { 
+        firstName: 'Mabel', 
+        lastName: 'Phil', 
+        studentId: 's1223', 
+        houseId: 'h1118'
+    })
+// Query to display the new doc added.
+db.students.findOne({firstName: 'Mabel'})
+
+// Query to delete the doc created
+db.students.deleteOne({firstName:"Mabel"})
+
+// Query to prove doc is no longer there
+db.students.findOne({firstName: 'Mabel'})
+
+// aggregate query to show list of students by house
+db.houses.aggregate([
+    { 
+        $lookup:{ 
+            from:"students", 
+            localField:"houseId",
+            foreignField:"houseId", 
+            as:"students" 
+        }
+    }
+])
+
+// aggregate query to show a list of students for house of
+// Gryffindor 
+db.houses.aggregate([
+    { 
+        $match : 
+        {
+            houseId:"h1007"
+        }
+    }, 
+    {
+        $lookup:
+        {
+            from:"students",
+            localField:"houseId",
+            foreignField:"houseId",
+            as:"house_Griff_students"
+        } 
+    }
+])
+
+//Query to show a list of students for the Eagle mascot
+//using $match and $lookup
+db.houses.aggregate(
+    [
+        {
+            $match:
+            {
+                houseId:"h1009"
+            }
+        }, 
+        {
+            $lookup:
+            {
+                from:"students", 
+                localField:"houseId",
+                foreignField:"houseId",
+                as:"eagle_mascot_student"
+            }
+        }
+    ])
---
+++ week_6/phiseme-userspl.py
@@ -0,0 +1,25 @@
+"""
+Title: phiseme-userspl.py
+Author: Manel Phiseme
+Date: 18 June 2022
+Description: Exercise 6.3
+"""
+
+""" import the MongoClient"""
+from pymongo import MongoClient
+
+""" build a connection string to connect to"""
+client = MongoClient("mongodb+srv://web335_user:s3cret@buwebdev-cluster-1.96qtg.mongodb.net/web335DBretryWrites=true&w=majority")
+
+"""configure a variable to access the web335DB"""
+db = client['web335DB']
+
+"""Python for loop to display all document in the user's collection"""
+for user in db.users.find():
+    print(user)
+
+"""Python code to display document with employeeID of 1011"""
+print(db.users.find_one({"employeeId":"1011"}))
+
+"""Python code to display document with last name Mozart"""
+print(db.users.find_one({"lastName":"Mozart"}))
